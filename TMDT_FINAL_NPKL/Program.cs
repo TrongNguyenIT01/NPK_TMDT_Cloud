b4b811b5cd7050d02163c98ec0934393b387229f
Phú <hoangphu24122005@gmail.com>
2026-08-10 14:59:32 +0700

    Chức năng quên mật

--- TMDT_FINAL_NPKL/Program.cs
+++ TMDT_FINAL_NPKL/Program.cs
@@ -26,6 +26,7 @@ object value = builder.Services.AddSwaggerGen();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddMemoryCache();
 builder.Services.AddDbContext<WebTmdtContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WEB_TMDT")));
 
 var app = builder.Build();
